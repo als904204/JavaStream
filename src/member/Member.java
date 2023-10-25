8092498b6ef0e57649931495e5d61832a50d549c
Min <als904204@naver.com>
2023-10-25 21:06:18 +0900

    add Member builder

--- src/member/Member.java
+++ src/member/Member.java
@@ -5,10 +5,9 @@ public class Member {
     private Long id;
     private String name;
 
-
-    public Member(Long id, String name) {
-        this.id = id;
-        this.name = name;
+    private Member(Builder builder) {
+        this.id = builder.id;
+        this.name = builder.name;
     }
 
     public Long getId() {
@@ -25,5 +24,31 @@ public class Member {
             "id=" + id +
             ", name='" + name + '\'' +
             '}';
+    }
+
+    // builder pattern
+    public static class Builder{
+        private Long id;
+        private String name;
+
+        public Builder id(Long id) {
+            this.id = id;
+            return this;
+        }
+
+        public Builder name(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public Member build() {
+            // 필요한 경우, 여기서 객체의 유효성 검사를 수행할 수 있습니다.
+            return new Member(this);
+        }
+
+    }
+
+    public static Builder builder() {
+        return new Builder();
     }
 }
