fe98316c9321fceb9b314e3867632d50a0c85279
Min <als904204@naver.com>
2023-10-26 15:58:08 +0900

    Member age column add

--- src/member/Member.java
+++ src/member/Member.java
@@ -5,15 +5,23 @@ public class Member {
     private Long id;
     private String name;
 
+    private int age;
+
     private Member(Builder builder) {
         this.name = builder.name;
+        this.age = builder.age;
     }
 
     public void setId(Long id) {
         this.id = id;
     }
+
     public Long getId() {
         return id;
+    }
+
+    public int getAge(){
+        return age;
     }
 
     public String getName() {
@@ -25,15 +33,22 @@ public class Member {
         return "Member{" +
             "id=" + id +
             ", name='" + name + '\'' +
+            ", age='" + age + '\'' +
             '}';
     }
 
     // builder pattern
     public static class Builder{
         private String name;
+        private int age;
 
         public Builder name(String name) {
             this.name = name;
+            return this;
+        }
+
+        public Builder age(int age) {
+            this.age = age;
             return this;
         }
 
